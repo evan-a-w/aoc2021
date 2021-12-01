04dd88cfc5a5032e0cf4f1a67cf9266f5600b515
Evan Williams <evanawilliams@live.com>
2021-12-01 19:10:14 +1100

    😊

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,9 +10,9 @@ p1p2 :: IO ()
 p1p2 = do
   inp <- readFile "inputs/p1p1.txt"
   let x = (map read) . words $ inp :: [Int]
-  putStrLn . show . length . filter
-    (\(a, b) -> b > a) $ let threes = zipWith (\(a, b) c -> [a,b,c])
-                                      (zipWith (,) x (tail x))
-                                      ((tail . tail) x)
-                             sums = map sum threes in
-                         zipWith (,) sums (tail sums)
+  putStrLn . show . length . filter (\(a, b) -> b > a) $
+    let threes = zipWith (\(a, b) c -> [a,b,c])
+                 (zipWith (,) x (tail x))
+                 ((tail . tail) x)
+        sums = map sum threes in
+    zipWith (,) sums (tail sums)
