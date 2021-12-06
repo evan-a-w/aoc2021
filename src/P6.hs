a1f3f2d6c8a5da2d2493a58fddf801ef88615e38
Evan Williams <evanawilliams@live.com>
2021-12-06 19:15:57 +1100

    😊

--- src/P6.hs
+++ src/P6.hs
@@ -2,6 +2,7 @@ module P6 where
 
 import Data.Function
 import Data.List
+import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Debug.Trace
 import P4 (split)
@@ -18,29 +19,23 @@ p6p1 = do
       end_fish = fst $ until (\(_, d) -> d == 80) inc_day (initial_fish, 0)
   putStrLn . show . length $ end_fish
 
+type Fish = Map Int Int
+
 p6p2 :: IO ()
 p6p2 = do
-  inp <- readFile "inputs/test.txt"
-  let initial_fish = map read . split ',' $ inp :: [Int]
-      inc cl num =
-        if num == 0
-          then 8 : 6 : cl
-          else num - 1 : cl
-      inc_day = inc []
-      changes_2_to_n n
-        | n == 0 = map inc_day
-        | otherwise = 
-      eight_changes =
-        map
-          (fst . until (\(_, d) -> d == 8) inc_day . (\x -> ([x], 0)))
-          [0 .. 8]
-      inc_eight (xs, i) = (concat . map (\x -> eight_changes !! x) $ xs, i + 8)
-      change256 =
-        map
-          (fst . until (\(_, d) -> d == 256) inc_eight . (\x -> ([x], 0)))
-          [0 .. 8]
-      inc_256 = concat . map (\x -> change256 !! x)
-      end_fish = inc_256 initial_fish
-  putStrLn . show . length $ end_fish
+  inp <- readFile "inputs/p6.txt"
+  let initial_list = map read . split ',' $ inp :: [Int]
+      initial_fish :: Fish
+      initial_fish =
+        foldr (\x y -> Map.insertWith (+) x 1 y) Map.empty initial_list
+      inc :: Int -> Int -> Fish -> Fish
+      inc k num m
+        | k == 0 =
+          let a = Map.insertWith (+) 8 num m
+           in Map.insertWith (+) 6 num a
+        | otherwise = Map.insertWith (+) (k - 1) num m
+      inc_day (xs, i) = (Map.foldrWithKey inc Map.empty xs, i + 1)
+      final_fish = fst $ until (\(_, d) -> d == 256) inc_day (initial_fish, 0)
+  putStrLn . show . foldr (+) 0 $ final_fish
 
 sol6 = [p6p1, p6p2]
