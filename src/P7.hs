90e6b672b32f3aaae18a9178c3152eb6b6c06628
Evan Williams <evanawilliams@live.com>
2021-12-07 19:52:16 +1100

    😊

--- src/P7.hs
+++ src/P7.hs
@@ -1,11 +1,35 @@
 module P7 where
 
+import Data.Function
+import Data.List
+import P4 (split)
+
 p7p1 :: IO ()
 p7p1 = do
-  putStrLn "pe"
+  inp <- readFile "inputs/p7.txt"
+  let crabs = map read . split ',' $ inp :: [Int]
+      sCrabs = sort crabs
+      med =
+        case length sCrabs `rem` 2 of
+          1 -> sCrabs !! (length sCrabs `div` 2)
+          _ ->
+            (sCrabs !! (length sCrabs `div` 2) +
+             sCrabs !! (length sCrabs `div` 2)) `div`
+            2
+  putStrLn . show . sum . map (\x -> abs (x - med)) $ crabs
 
 p7p2 :: IO ()
 p7p2 = do
-  putStrLn "pe"
+  inp <- readFile "inputs/p7.txt"
+  let crabs = map read . split ',' $ inp :: [Int]
+      cost i =
+        sum .
+        map
+          (\x ->
+             let p = abs (x - i)
+              in p * (p + 1) `div` 2) $
+        crabs
+      res = minimum $ cost <$> [minimum crabs .. maximum crabs]
+  putStrLn . show $ res
 
 sol7 = [p7p1, p7p2]
